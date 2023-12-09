1b6501537e0e200359ef81a62fa5632ecf2f3b1d
Atticuszz <1831768457@qq.com>
2023-12-09 15:51:14 +0800

    client in pyqt and fastapi ready to accept socket
    Signed-off-by: Atticus Zhou <1831768457@qq.com>

--- tests/aynic_test.py
+++ tests/aynic_test.py
@@ -1,9 +1,10 @@
 # coding=utf-8
+import asyncio
 import os
 
 import pytest
 from dotenv import load_dotenv
-from gotrue import AuthResponse, UserResponse, Session
+from gotrue import AuthResponse, UserResponse, Session, UserAttributes
 
 from supabase_py_async import create_client
 
@@ -68,9 +69,40 @@ async def test_operate_with_token():
     print("after set_session with user_1,response_5.user", response_4.user.email)
     print("after set_session with user_1,called set_session user1", await client.auth.get_user())
 
+    response_5: AuthResponse = await client.auth.refresh_session(response_1.session.refresh_token)
+
+    # check token difference
+    if response_1.session.access_token != response_5.session.access_token:
+        print("refresh access_token success")
+    else:
+        print("refresh access_token failed")
+    if response_1.session.refresh_token != response_5.session.refresh_token:
+        print("refresh refresh_token success")
+    else:
+        print("refresh refresh_token failed")
+
+    # check token is valid
+    response_6: AuthResponse = await client.auth.set_session(response_5.session.access_token,
+                                                             response_5.session.refresh_token)
+    print("after set_session with user_1,response_6.user", response_6.user.email)
+    print("after set_session with user_1,called set_session user1", await client.auth.get_user())
+
+
+async def update_user():
+    # 获取test.env中的环境变量
+    url = os.getenv("SUPABASE_TEST_URL")
+    key = os.getenv("SUPABASE_TEST_KEY")
+    # 创建客户端
+    client = await create_client(url, key)
+    response_1: AuthResponse = await client.auth.sign_in_with_password(
+        {'email': 'zhouge1831@gmail.com', 'password': 'Zz030327#'}
+    )
+    user_r = response_1.session.user
+    user_attr = UserAttributes(data={"name": "zhouge"})
+    await client.auth.update_user(user_attr)
+
 
 if __name__ == '__main__':
     # asyncio.run(test_sign_in())
     #     # asyncio.run(sign_in())
-    # asyncio.run(test_operate_with_token())
-    pass
+    asyncio.run(update_user())
