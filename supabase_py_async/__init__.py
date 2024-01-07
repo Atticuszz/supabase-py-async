89084931bb18752b9b9fdbb26c9c0f5f402036e8
Atticuszz <1831768457@qq.com>
2024-01-07 14:34:50 +0800

    release 2.3.1

--- supabase_py_async/__init__.py
+++ supabase_py_async/__init__.py
@@ -1,4 +1,5 @@
-from .__version__ import __version__
 from .client_async import AsyncClient, create_client
 from .lib.auth_client import AsyncSupabaseAuthClient as SupabaseAuthClient
 from .lib.realtime_client import SupabaseRealtimeClient
+
+__version__ = "2.3.1"
