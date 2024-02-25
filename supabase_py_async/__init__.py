4b4cb7d1901ce414ec15ce0da026d57412cd7dc6
atticuszz <1831768457@qq.com>
2024-02-25 14:01:13 +0800

    fix: Export Core Supabase Classes and Functions Explicitly via __all__

--- supabase_py_async/__init__.py
+++ supabase_py_async/__init__.py
@@ -1,4 +1,22 @@
+from postgrest import APIError as PostgrestAPIError
+from postgrest import APIResponse as PostgrestAPIResponse
+from storage3.utils import StorageException
+
 from .__version__ import __version__
-from .client_async import AsyncClient, ClientOptions, create_client
+from .client_async import AsyncClient
+from .client_async import AsyncStorageClient as SupabaseStorageClient
+from .client_async import ClientOptions, create_client
 from .lib.auth_client import AsyncSupabaseAuthClient as SupabaseAuthClient
 from .lib.realtime_client import SupabaseRealtimeClient
+
+__all__ = [
+    "create_client",
+    "AsyncClient",
+    "SupabaseAuthClient",
+    "SupabaseStorageClient",
+    "SupabaseRealtimeClient",
+    "PostgrestAPIError",
+    "PostgrestAPIResponse",
+    "StorageException",
+    "__version__",
+]
